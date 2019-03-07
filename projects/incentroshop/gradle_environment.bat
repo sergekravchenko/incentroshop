722da3f02db701363bc1b29c4df15f9e2e479675
Serge Kravchenko <skravchenko1991@gmail.com>
2019-03-07 10:11:24 +0100

    [UPDATE FILES]

---
+++ projects/incentroshop/gradle_environment.bat
@@ -0,0 +1,36 @@
+@echo off
+REM Layout of development environment. Adapt it to fit your needs.
+REM The default layout assumes, to have a single top level directory
+REM DEVELOPER_BASE, with all other required directories nested into it.
+
+REM DEVELOPER_BASE is only needed for default layout
+SET DEVELOPER_BASE=%~dp0
+
+SET SERVER=%DEVELOPER_BASE%\build\server
+SET WORKSPACE=%USERPROFILE%\incentroshop\workspace
+
+REM Set Gradle project properties for all directories that Gradle needs to know
+SET ORG_GRADLE_PROJECT_serverDirectory=%SERVER%
+SET ORG_GRADLE_PROJECT_sourceDirectories=%DEVELOPER_BASE%
+SET ORG_GRADLE_PROJECT_buildEnvironmentPropertiesSample=%USERPROFILE%\incentroshop\environment.properties.sample
+
+REM Set aliases
+doskey cdServer=cd/d %SERVER%
+doskey openStudio=c:\intershop\IntershopStudio\IntershopStudio.exe
+
+echo #
+echo Gradle environment is set up.
+echo The following aliases are available
+echo -------------------------------------------------------
+echo cdServer   - change directory to server directory
+echo -------------------------------------------------------
+echo openStudio - open Intershop Studio
+echo -------------------------------------------------------
+echo #
+
+IF EXIST %SERVER%\local\bin\environment.bat (
+CALL %SERVER%\local\bin\environment.bat
+echo Legacy Ant environment is set up. ^(Called %SERVER%\local\bin\environment.bat.^)
+) ELSE (
+echo Legacy Ant environment is not set up. ^(Please call %SERVER%\local\bin\environment.bat after deployment.^)
+)
---
+++ projects/incentroshop/gradle_environment.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# ---------------------------------------------
+# Calculate the correct home dir of the script
+
+progName='gradle_environment.sh'
+prog=$0
+
+notsourced=$(echo $prog|grep $progName)
+
+if [ -z "$notsourced" ]
+then
+  prog=$BASH_SOURCE
+else
+  echo "The script is not being sourced! Please call the script 'source $prog' or '. $prog'"
+fi
+
+realpath=$(readlink -f "$prog")
+
+if test ! -f "$realpath"; then
+  echo >&2 "Cannot determine home directory. Please call this script with 'source /absolute/path/to/$progName'"
+fi
+
+# Layout of development environment. Adapt it to fit your needs.
+# The default layout assumes, to have a single top level directory
+# DEVELOPER_BASE, with all other required directories nested into it.
+
+# DEVELOPER_BASE is only needed for default layout
+# ---------------------------------------------
+DEVELOPER_BASE=`dirname "$realpath"`
+# ---------------------------------------------
+
+SERVER=${DEVELOPER_BASE}/build/server
+WORKSPACE=${HOME}/incentroshop/workspace
+
+# Set Gradle project properties for all directories that Gradle needs to know
+ORG_GRADLE_PROJECT_serverDirectory=${SERVER}
+ORG_GRADLE_PROJECT_sourceDirectories=${DEVELOPER_BASE}
+ORG_GRADLE_PROJECT_buildEnvironmentPropertiesSample=${HOME}/incentroshop/environment.properties.sample
+
+export DEVELOPER_BASE SERVER WORKSPACE REPO ORG_GRADLE_PROJECT_serverDirectory ORG_GRADLE_PROJECT_sourceDirectories ORG_GRADLE_PROJECT_buildEnvironmentPropertiesSample
+
+# set aliases 
+alias cdServer="cd $SERVER"
+alias openStudio="/opt/intershop/IntershopStudio/IntershopStudio"
+
+echo " "
+echo "Gradle environment is set up."
+echo "The following aliases are available"
+echo "-------------------------------------------------------"
+echo "cdServer   - change directory to server directory"
+echo "-------------------------------------------------------"
+echo "openStudio - open Intershop Studio"
+echo "-------------------------------------------------------"
+echo " "
+
+if [ -f ${SERVER}/local/bin/environment.sh ]
+then
+	. ${SERVER}/local/bin/environment.sh
+	echo "Legacy Ant environment is set up - (${SERVER}/local/bin/environment.sh executed)"
+else
+	echo "Legacy Ant environment is not set up - Please execute [. ${SERVER}/local/bin/environment.sh] after deployment."
+fi
